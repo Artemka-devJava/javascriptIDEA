33d0589528c1455931cf0df73a077eed8c12f753
artemka23345 <144363632+artemka23345@users.noreply.github.com>
2024-11-21 13:08:46 +0300

    Строгое сравнение

--- 00-javascript/app.js
+++ 00-javascript/app.js
@@ -4,3 +4,10 @@ const name = "Artem";
 let name1 = "Serega";
 name1 = "tol9n";
 console.log(age, name, name1);
+
+console.log("is 5 > 3",5>3);
+//Сравниваем через == число и строку получаем true
+// надо сравнивать через строгое сравнение "==="
+
+console.log( 5 == "5"); //true
+console.log( 5 === "5"); //false
