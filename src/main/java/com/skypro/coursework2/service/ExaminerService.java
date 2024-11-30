b0ca8d42f2d64748a2513072685c3a4e37555220
artemka23345 <144363632+artemka23345@users.noreply.github.com>
2024-11-30 12:35:36 +0300

    Починил

--- src/main/java/com/skypro/coursework2/service/ExaminerService.java
+++
@@ -1,9 +0,0 @@
-package com.skypro.coursework2.service;
-
-import com.skypro.coursework2.model.Question;
-
-import java.util.Collection;
-
-public interface ExaminerService {
-    Collection<Question> getQuestions(int amount);
-}
